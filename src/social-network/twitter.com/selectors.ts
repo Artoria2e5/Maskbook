7731de26ba69253f48bd9046e5f7963f48213b68
Misaka <447f.misaka@outlook.com>
2019-07-26 15:29:27 +0800

    feat: tl selector

--- src/social-network/twitter.com/selectors.ts
+++ src/social-network/twitter.com/selectors.ts
@@ -11,21 +11,32 @@ const querySelector = (selector: string) => {
  * userName   screenName
  */
 
+const base = querySelector('script')
 const name = /"session":{.*?"user":{.*?"screen_name":"(.*?)","name":"(.*?)"}}/
 const bio = /"entities":{.*?"users":{.*?"entities":{.*?"[0-9]*":{.*?"description":"(.*?)"/
-const base = querySelector('script')
+const avatar = /"entities":{.*?"users":{.*?"entities":{.*?"[0-9]*":{.*?"profile_image_url_https":"(.*?)"/
 
 const factory = (regex: RegExp, index?: number) => {
     return base.clone().map(x => regexMatch(x.innerText, regex, index))
 }
 
-export const screenNameSelector = factory(name, 0);
-export const userNameSelector = factory(name, 1);
-export const userBioSelector = factory(bio, 0);
+export const screenNameSelector = factory(name, 0)
+export const userNameSelector = factory(name, 1)
+export const userBioSelector = factory(bio, 0)
+export const userAvatarUrlSelector = factory(avatar, 0)
+
+const newPostEditorSelectorString =
+    '[role="main"] [role="progressbar"] + div .DraftEditor-root .public-DraftEditorPlaceholder-root'
+
+export const newPostEditorSelector = querySelector(newPostEditorSelectorString)
+export const newPostEditorOnFocusSelector = querySelector(
+    '[role="main"] [role="progressbar"] + div .DraftEditor-root .public-DraftEditorPlaceholder-hasFocus'
+)
 
 /**
- * This selector will got an element if new post editor window has focus.
+ *  Tested on main-timeline, trending-timeline
+ *  TODO: Missing test for list
  */
-export const newPostEditorSelector = querySelector(
-    '[role="main"] [role="progressbar"] + div .DraftEditor-root .public-DraftEditorPlaceholder-hasFocus'
+export const timelineSelector = querySelector(
+    '[role="main"] [data-testid="primaryColumn"] section > div > div > div > *'
 )

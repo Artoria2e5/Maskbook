aab94488c51f6a3d771c36b4c93d53cbb067ecdc
Misaka <447f.misaka@outlook.com>
2019-07-26 14:58:31 +0800

    feat: new post editor selector, renaming

--- src/social-network/twitter.com/selectors.ts
+++ src/social-network/twitter.com/selectors.ts
@@ -1,6 +1,10 @@
 import { LiveSelector } from '@holoflows/kit'
 import { regexMatch } from '../../utils/utils'
 
+const querySelector = (selector: string) => {
+    return new LiveSelector().querySelector<HTMLAnchorElement>(selector)
+}
+
 /**
  * @naming
  * 御坂       (@Misaka_xxxx)
@@ -9,7 +13,7 @@ import { regexMatch } from '../../utils/utils'
 
 const name = /"session":{.*?"user":{.*?"screen_name":"(.*?)","name":"(.*?)"}}/
 const bio = /"entities":{.*?"users":{.*?"entities":{.*?"[0-9]*":{.*?"description":"(.*?)"/
-const base = new LiveSelector().querySelector<HTMLAnchorElement>('script')
+const base = querySelector('script')
 
 const factory = (regex: RegExp, index?: number) => {
     return base.clone().map(x => regexMatch(x.innerText, regex, index))
@@ -18,3 +22,10 @@ const factory = (regex: RegExp, index?: number) => {
 export const screenNameSelector = factory(name, 0);
 export const userNameSelector = factory(name, 1);
 export const userBioSelector = factory(bio, 0);
+
+/**
+ * This selector will got an element if new post editor window has focus.
+ */
+export const newPostEditorSelector = querySelector(
+    '[role="main"] [role="progressbar"] + div .DraftEditor-root .public-DraftEditorPlaceholder-hasFocus'
+)

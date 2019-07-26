c13d4a1f13bfe8b2e5730906b6bc35d01adefdb0
Misaka <447f.misaka@outlook.com>
2019-07-26 11:51:45 +0800

    fix: use inline script instead of visible element

--- src/social-network/twitter.com/user.ts
+++ src/social-network/twitter.com/user.ts
@@ -7,18 +7,12 @@ import { regexMatch } from '../../utils/utils'
  * userName   screenName
  */
 
-export const screenNameSelector = new LiveSelector()
-    .querySelector<HTMLAnchorElement>('[aria-label="Primary"][role="navigation"] [aria-label="Profile"]')
-    .map(x => regexMatch(x.href, /(twitter\.com\/)(.*)/, 2))
+const name = /"user":{"screen_name":"(.*?)","name":"(.*?)"}}/
 
-export const screenNameSelectorMobile = new LiveSelector()
+export const screenNameSelector = new LiveSelector()
     .querySelector<HTMLAnchorElement>('script')
-    .map(x => regexMatch(x.innerText, /screen_name":"(.*?)"/, 0))
+    .map(x => regexMatch(x.innerText, name, 0))
 
 export const userNameSelector = new LiveSelector()
-    .querySelector('[role="main"] [role="progressbar"]+* [aria-label]')
-    .map(x => x.getAttribute("aria-label"))
-
-export const userNameSelectorMobile = new LiveSelector()
     .querySelector('script')
-    .map(x => regexMatch(x.innerText, /(screen_name).{0,128}name":"(.*?)"/, 1))
+    .map(x => regexMatch(x.innerText, name, 1));
